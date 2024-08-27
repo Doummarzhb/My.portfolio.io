8c09fd51c43370fe332a81e5405f652384d1eadf
Doummar M.alzahabi <98951369+Doummarzhb@users.noreply.github.com>
2024-08-28 01:16:09 +0300

    تحديث ملف index.html و vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './My.portfolio.io/',  // ضع هنا اسم المستودع الخاص بك
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
+  base: '/My.portfolio.io/',   
 })
