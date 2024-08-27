0e4c779866f9eb26093bd96bbdd5557d168f97a8
Doummar M.alzahabi <98951369+Doummarzhb@users.noreply.github.com>
2024-08-28 01:21:52 +0300

    a

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/My.portfolio.io/',   
+  base: '/My.portfolio.io/',  // تأكد من أن هذا هو اسم المستودع على GitHub
 })
